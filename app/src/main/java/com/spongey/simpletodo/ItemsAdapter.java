f6dd18e2eb57f271235e04482ffeb48b20d8cc90
ChristineWasike <christinewasike22@gmail.com>
2020-07-12 11:53:52 +0300

    Rendered dummy data to the list

---
+++ app/src/main/java/com/spongey/simpletodo/ItemsAdapter.java
@@ -0,0 +1,64 @@
+package com.spongey.simpletodo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+// Responsible for displaying data from the model into a single row in the recyclerview at a time
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+
+    List<String> items;
+
+    public ItemsAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Use  layout inflater  to inflate the view
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1,
+                parent, false);
+        // wrap it  inside a ViewHolder and return it
+        return new ViewHolder(todoView);
+    }
+
+    // Responsible for binding data to a particular view  holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Grab the item at the position
+        String item = items.get(position);
+
+        // Bind the item into the specific view holder
+        holder.bind(item);
+    }
+
+    // Tells the recycler view how many items are in the list
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    // Container to provide easy access  to views  that represent each of the rows in teh list
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvItem;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        // Responsible for updating teh view in the view holder with its data
+        public void bind(String item) {
+            tvItem.setText(item);
+        }
+    }
+}
--- app/src/main/java/com/spongey/simpletodo/MainActivity.java
+++ app/src/main/java/com/spongey/simpletodo/MainActivity.java
@@ -1,14 +1,44 @@
 package com.spongey.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<String> items;
+
+    // References to the views in the main activity
+
+    Button addButton;
+    EditText editItem;
+    RecyclerView rvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Declaration of the items in the main layout
+        addButton = findViewById(R.id.addButton);
+        editItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Go to the gym");
+        items.add("Do the morning dishes");
+        items.add("Have fun");
+
+        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
     }
 }

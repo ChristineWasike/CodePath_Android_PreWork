65b72fa257d1420ecbc0f4d325bb9816f3c4cc70
ChristineWasike <christinewasike22@gmail.com>
2020-07-12 12:25:21 +0300

    Implemented persistence  with the load model from file system on app startup and store to file system

--- app/src/main/java/com/spongey/simpletodo/MainActivity.java
+++ app/src/main/java/com/spongey/simpletodo/MainActivity.java
@@ -5,9 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     Button addButton;
     EditText editItem;
     RecyclerView rvItems;
+    ItemsAdapter itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +40,60 @@ public class MainActivity extends AppCompatActivity {
         rvItems = findViewById(R.id.rvItems);
 
 
-        items = new ArrayList<>();
-        items.add("Buy milk");
-        items.add("Go to the gym");
-        items.add("Do the morning dishes");
-        items.add("Have fun");
+        loadItems();
 
-        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+
+        ItemsAdapter.OnLongClickListener longClickListener = new ItemsAdapter.OnLongClickListener() {
+            @Override
+            public void onItemLongClicked(int position) {
+                // Remove the item from the recycler view
+                items.remove(position);
+                // Notify adapter on delete
+                itemsAdapter.notifyItemRemoved(position);
+                Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        };
+
+        itemsAdapter = new ItemsAdapter(items, longClickListener);
         rvItems.setAdapter(itemsAdapter);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String todoItem = editItem.getText().toString();
+                // Add the item to the model
+                items.add(todoItem);
+                // Notify the adapter of the addition
+                itemsAdapter.notifyItemInserted(items.size() - 1);
+                editItem.setText("");
+                Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        });
+    }
+
+    private File getDataFile(){
+        return new File(getFilesDir(), "data.txt");
+    }
+    //Loads items by reading  every line in teh data file
+    private  void loadItems(){
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading items", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    // Saves items to the data file
+    private void saveItems(){
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("MainActivity", "Error writing items", e);
+        }
     }
 }
